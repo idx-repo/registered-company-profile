81a74c822531b0777c034f06398dbd5f486b1af4
Joko Putranto <akun.joko.putranto@gmail.com>
2014-08-11 18:53:58 +0700

    layout & config

--- www/protected/views/layouts/main.php
+++ www/protected/views/layouts/main.php
@@ -48,7 +48,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+		Copyright &copy; <?php echo date('Y'); ?> by Three Musketeers.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
